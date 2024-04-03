a24c90a34568aae9f2e10b87c1790f5bd0d9de0f
Michel <henriquemichel10@gmail.com>
2024-04-03 20:16:53 -0300

    Foi

---
+++ AppRpgEtec/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace AppRpgEtec
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            string login = Preferences.Get("UsuarioUsername", string.Empty);
+            lblLogin.Text = $"Login: {login}";
+        }
+    }
+}
