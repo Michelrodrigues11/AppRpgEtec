bf5b8d0062da8680eb8c832c64819e3056c776cd
Michel <henriquemichel10@gmail.com>
2024-04-03 20:36:07 -0300

    Foi

--- AppRpgEtec/Services/Request.cs
+++ AppRpgEtec/Services/Request.cs
@@ -14,7 +14,8 @@ namespace AppRpgEtec.Services
         {
             HttpClient httpClient = new HttpClient();
 
-            httpClient.DefaultRequestHeaders.Authorization
+
+            httpClient.DefaultRequestHeaders.Authorization 
             = new AuthenticationHeaderValue("Bearer", token);
 
             var content = new StringContent(JsonConvert.SerializeObject(data));
@@ -48,8 +49,47 @@ namespace AppRpgEtec.Services
                 throw new Exception(serialized);
 
             return result;
-        }        
+        }
+
+        public async Task<int> PutAsync<TResult>(string uri, TResult data, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PutAsync(uri, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
 
 
+        public async Task<TResult> GetAsync<TResult>(string uri, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+            TResult result = await Task.Run(() =>
+            JsonConvert.DeserializeObject<TResult>(serialized));
+            throw new Exception(serialized);
+        }
+
+        public async Task<int> DeleteAsync(string uri, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new
+            AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await httpClient.DeleteAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
     }
 }
